aacc9ed9a4abbdd3ebe0967ed4a44244e0abcfc0
DjGusaas <gusaa004@morris.umn.edu>
2016-01-28 03:48:12 +0000

    Intial commit

---
+++ src/Santa/Main.java
@@ -0,0 +1,10 @@
+package Santa;
+
+public class Main {
+	public static void main(String args[]) {
+		Santa Mary = new Santa("Mary", "Pat", "Jon");
+		Santa Pat = new Santa("Pat", "Mary", "");
+		
+		System.out.println(Mary.getName());
+	}
+}
