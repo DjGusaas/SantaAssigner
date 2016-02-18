ccefa23c3dfedd5adb1eaea74711f6fb6b9f3cf6
DjGusaas <gusaa004@morris.umn.edu>
2016-02-18 03:10:44 +0000

    Added most of random search as well as some helper functions

--- src/SantaSelector/Main.java
+++ src/SantaSelector/Main.java
@@ -1,12 +1,22 @@
 package SantaSelector;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String args[]) {
 		Santa Susan = new Santa("Susan", "'Bob", "Jon");
 		Santa Bob = new Santa("Bob", "Susan", "Steve");
-		Santa Steve = new Santa("Steve", "", "");
+		Santa Steve = new Santa("Steve", "", "Pat");
 		Santa Jon = new Santa("Jon", "", "Susan");
 		
-		Santa.randomSelector();
+//		Santa.randomSelector();
+//		ArrayList<Santa> test = new ArrayList<Santa>();
+//		test.add(Susan);
+//		ArrayList<Santa> copy = Santa.deepClone(test);
+//		System.out.println((copy.get(0)).getName());
+		
+		ArrayList<Santa> test= Santa.randomSelector();
+		
+		System.out.println(test.get(0));
 	}
 }
--- src/SantaSelector/Santa.java
+++ src/SantaSelector/Santa.java
@@ -7,7 +7,7 @@ public class Santa {
 	private String name;
 	private String partner;
 	private String previous;
-	private String future;
+	private String giftee;
 
 	public static ArrayList<Santa> group = new ArrayList<Santa>();
 	
@@ -15,33 +15,95 @@ public class Santa {
 		this.name = name;
 		this.partner = partner;
 		this.previous = previous;
-		this.future = "";
+		this.giftee = "";
 
 		group.add(this);
 	}
 	
+	///////////////////////////
+	/// Getters and Setters ///
+	///////////////////////////
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getPartner() {
+		return this.partner;
+	}
+	
+	public String getPrevious() {
+		return this.previous;
+	}
+	
+	public String getGiftee() {
+		return this.giftee;
+	}
+	
+	public void setGiftee(String s) {
+		this.giftee = s;
+	}
+	
+	//////////////////////////////
+	/// Santa helper functions ///
+	//////////////////////////////
+	
+	public void printSanta(Santa s) {
+		System.out.println(s.getName() + " gifts: " + s.getGiftee());
+	}
+	
+	public void printGroup(ArrayList<Santa> g) {
+		for (Santa s : g) {
+			printSanta(s);
+		}
+	}
+	
+	/////////////////////
+	/// Random Search ///
+	/////////////////////
+	
 	public static ArrayList<Santa> randomSelector() {
-		ArrayList<Santa> temp = new ArrayList<Santa>();
+		ArrayList<Santa> work = deepClone(group);
+		ArrayList<Santa> output = new ArrayList<Santa>();
 		
-		for (Santa s : group) {
-			if (s.hasValidRecipient()) {
-				findRandom(s);
-				temp.add(s);
+		for (Santa s : work) {
+			if (hasPotentialGiftee(s, work)) {
+				findRandom(s, work);
+				output.add(s);
 			} else {
-				// TODO 
+				randomSelector();
 			}
 		}
-		return temp;
+		return output;
+	}
+	
+	public static Santa findRandom(Santa s, ArrayList<Santa> a) {
+		Random rand = new Random();
+		
+		while(s.getGiftee() == "") {
+			String attempt = a.get(rand.nextInt(a.size())).getName();
+			if (attempt != s.getPartner() && attempt != s.getPrevious()) {
+				s.setGiftee(attempt);
+			}
+		}
+		return s;
 	}
 
-	public boolean hasValidRecipient() {
+	///////////////////////////////
+	/// Search helper functions ///
+	///////////////////////////////
+	
+	public static boolean hasPotentialGiftee(Santa s, ArrayList<Santa> a) {
 		// TODO
 		return false;
 	}
-
-	public static Santa findRandom(Santa s) {
-		Random rand;
-		// TODO
-		return s;
-	}	
+	
+	public static ArrayList<Santa> deepClone(ArrayList<Santa> input) {
+		ArrayList<Santa> output = new ArrayList<Santa>();
+		
+		for (Santa s : input) {
+			output.add(s);
+		}
+		return output;
+	}
 }
